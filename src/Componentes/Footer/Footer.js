9fcc952f2a80154897123545b67cc2cb23b27151
JimenaKatuMartinez <93katu@gmail.com>
2022-04-25 19:34:40 -0300

    Solucionado enlaces de footer

--- src/Componentes/Footer/Footer.js
+++ src/Componentes/Footer/Footer.js
@@ -1,4 +1,4 @@
-import React from "react";
+import { Link } from "react-router-dom";
 import "./Footer.css";
 
 export default function Footer() {
@@ -7,9 +7,15 @@ export default function Footer() {
       <footer className="row">
         <section className="legales col-12 col-lg-4">
           <ul>
-            <li key="1">Avisos legales</li>
-            <li key="2">Información corporativa</li>
-            <li key="3">Términos de uso</li>
+            <Link to="/avisos" className="link">
+              <li key="1">Avisos legales</li>
+            </Link>
+            <Link to="/infocorp" className="link">
+              <li key="2">Información corporativa</li>
+            </Link>
+            <Link to="/terminos" className="link">
+              <li key="3">Términos de uso</li>
+            </Link>
           </ul>
         </section>
         <section className="redes col-12 col-lg-4">
@@ -33,11 +39,7 @@ export default function Footer() {
               </a>
             </li>
             <li key="6">
-              <a
-                href="https://twitter.com/"
-                target="_blank"
-                rel="noreferrer"
-              >
+              <a href="https://twitter.com/" target="_blank" rel="noreferrer">
                 <i className="fab fa-twitter"></i>
               </a>
             </li>
@@ -45,9 +47,15 @@ export default function Footer() {
         </section>
         <section className="otros col-12 col-lg-4">
           <ul>
-            <li key="7">Contáctanos</li>
-            <li key="8">Centro de ayuda</li>
-            <li key="9">Privacidad</li>
+            <Link to="/Contactanos" className="link">
+              <li key="7">Contáctanos</li>
+            </Link>
+            <Link  to="/ayuda" className="link">
+              <li key="8">Centro de ayuda</li>
+            </Link>
+            <Link  to="/privacidad" className="link">
+              <li key="9">Privacidad</li>
+            </Link>
           </ul>
         </section>
       </footer>
